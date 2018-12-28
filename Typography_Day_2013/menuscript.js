c8329862a2ea81a875375c5e20d9e189446ff5bb
Rounak Parihar <42077172+Rounak14@users.noreply.github.com>
2018-12-28 11:43:37 +0530

    Add files via upload

---
+++ Typography_Day_2013/menuscript.js
@@ -0,0 +1,9 @@
+
+    $(document).ready(function() {
+	$(".nav_item").mouseover(function() {
+					$(this).toggleClass('nav_item_active').toggleClass('nav_item');
+										  });
+	$(".nav_item").mouseout(function() {
+					$(this).toggleClass('nav_item_active').toggleClass('nav_item');
+										  });
+							   });
